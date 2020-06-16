a6c48e1a4d5f0d3523be90eac15a545ce8f26fb4
Trojan0523 <1153532327@qq.com>
2020-06-16 21:51:44 +0800

    使用chai.expect 添加四个测试用例， 第五个需要mock

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Group from "./button-group"
 
 Vue.component('g-button', Button);
 Vue.component('t-icon', Icon);
-Vue.component('g-button-group',Group);
+Vue.component('g-button-group', Group);
 new Vue({
     el: '#app',
     data: {
@@ -15,3 +15,76 @@ new Vue({
     },
     method: {}
 })
+import chai from 'chai';
+
+const expect = chai.expect;
+// 单元测试
+/*
+*   当前测试icon:setting路径是否正确
+* */
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#isettings')
+    button.$el.remove();
+    button.$destroy();
+}
+/*
+*   当前测试icon:loading是否加载正确
+* */
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#iloading')
+    vm.$el.remove();
+    vm.$destroy();
+}
+/*
+*   当前测试icon:position是否正确
+* */
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    vm.$mount(div);
+    let svg = vm.$el.querySelector('svg');
+    let {order} = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    //  mock
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    });
+    vm.$mount();
+    vm.$on('click',function() {
+        console.log(1);
+    })
+    let button = vm.$el
+    button.click();
+}
