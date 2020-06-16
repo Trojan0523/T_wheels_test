1dfe16c813f6d68bcff79d6b0f369389e769f683
Trojan0523 <1153532327@qq.com>
2020-06-16 15:16:25 +0800

    验证iconPosition 为left 或right中的一个

--- src/button.vue
+++ src/button.vue
@@ -13,7 +13,17 @@
   import Vue from 'vue';
 
   export default {
-    props:['icon', 'iconPosition']
+    // props:['icon', 'iconPosition']
+    props: {
+      icon:{},
+      iconPosition:{
+        type: String,
+        default: 'left',
+        validator(value) {
+          return !(value !== 'left' && value !== 'right');
+        }
+      }
+    }
   };
 </script>
 
