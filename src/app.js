1aa28b1acd7820e8a7df988b0ad6c1e40b374469
Trojan0523 <1153532327@qq.com>
2020-06-19 13:44:54 +0800

    新增 col 和 row 组件

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import Button from "./button";
 import Icon from "./icon"
 import Group from "./button-group"
 import Input from "./input";
-
+import Row from  "./row"
+import Col from "./col"
 Vue.component('g-button', Button);
 Vue.component('t-icon', Icon);
 Vue.component('g-button-group', Group);
 Vue.component('t-input',Input);
+Vue.component('t-row',Row);
+Vue.component('t-col', Col)
 new Vue({
     el: '#app',
     data: {
---
+++ src/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background: gray;
+        width: 50%;
+        border: 1px solid red;
+    }
+
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+    /* for each &col_#{n} */
+        .#{$class-prefix}-#{&n} {
+            width: ($n / 24) * 100%;
+        }
+    }
+</style>
