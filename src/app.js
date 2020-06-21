3c06919722f078702ec0618c48d59e4e1a8f3329
Trojan0523 <1153532327@qq.com>
2020-06-22 00:11:52 +0800

    添加 toast 和 plugin

--- src/app.js
+++ src/app.js
@@ -3,25 +3,29 @@ import Button from "./button";
 import Icon from "./icon"
 import Group from "./button-group"
 import Input from "./input";
-import Row from  "./row"
+import Row from "./row"
 import Col from "./col"
 import Layout from "./layout"
 import Header from "./header"
 import Content from "./content"
 import Sider from "./sider"
 import Footer from "./footer"
+import Toast from "./toast"
+import plugin from "./plugin";
 
 Vue.component('g-button', Button);
 Vue.component('t-icon', Icon);
 Vue.component('g-button-group', Group);
-Vue.component('t-input',Input);
-Vue.component('t-row',Row);
+Vue.component('t-input', Input);
+Vue.component('t-row', Row);
 Vue.component('t-col', Col)
-Vue.component('t-layout',Layout)
-Vue.component('t-header',Header)
-Vue.component('t-content',Content)
-Vue.component('t-sider',Sider)
-Vue.component('t-footer',Footer)
+Vue.component('t-layout', Layout)
+Vue.component('t-header', Header)
+Vue.component('t-content', Content)
+Vue.component('t-sider', Sider)
+Vue.component('t-footer', Footer)
+Vue.component('t-toast', Toast)
+Vue.use(plugin)
 new Vue({
     el: '#app',
     data: {
@@ -30,9 +34,15 @@ new Vue({
         loading3: false,
         message: 'h1'
     },
+    created() {
+
+    },
     methods: {
         inputChange(e) {
             console.log(e.target.value);
+        },
+        showToast() {
+            this.$toast('我是message')
         }
     }
 })
---
+++ src/toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'TToast',
+    }
+</script>
+<style lang="scss" scoped>
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0,0,0,0.75);
+    $box-shadow: 0 0 3px;
+    .toast {
+        color: white;
+        background: $toast-bg;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: $font-size;
+        line-height: 1.8;
+        height: $toast-height;
+        display: flex;
+        align-items: center;
+        box-shadow: $box-shadow;
+        padding: 0 16px;
+    }
+</style>
