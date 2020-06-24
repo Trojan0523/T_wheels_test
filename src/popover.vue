eafbbeeb045431f8135107dd61fd1c31aa9ddf63
Trojan0523 <1153532327@qq.com>
2020-06-24 10:29:59 +0800

    实现最简单的 popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content"></slot>
         </div>
-        <slot ></slot>
+        <slot></slot>
     </div>
 </template>
 
@@ -15,7 +15,19 @@
         },
         methods: {
             xxx() {
-            this.visible = !this.visible
+                this.visible = !this.visible
+                if (this.visible === true) {
+                    setTimeout(() => {
+                        let eventHandler = () => {
+                            this.visible = false
+                            console.log('document 隐藏了 popover');
+                            document.removeEventListener('click', eventHandler)
+                        }
+                        document.addEventListener('click', eventHandler)
+                    })
+                } else {
+                    console.log('vm 隐藏popover');
+                }
             }
         }
     }
@@ -25,12 +37,13 @@
     .popover {
         display: inline-block;
         vertical-align: top;
+
         .content-wrapper {
-            position: absolute;
+            position: relative;
             bottom: 100%;
             left: 0;
             border: 1px solid red;
-            box-shadow: 0 0 3px rgba(0,0,0,0.5);
+            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
         }
     }
 </style>
