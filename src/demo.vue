c63753385f6ddc3cf4102ad8a9ce1340a03deb40
trojan0523 <1153532327@qq.com>
2020-12-18 23:17:36 +0800

    feature: use vue/cli to replace parcel

---
+++ src/demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <t-button icon="thumbs-up" icon-position="left">点赞</t-button>
+  </div>
+</template>
+
+<script>
+import Button from '@/button';
+export default {
+  name: 'demo',
+  components: {
+    't-button': Button
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
