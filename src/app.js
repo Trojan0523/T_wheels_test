6abc2092824cdd0bda3a2457cf0da120abaaf3ef
trojan0523 <1153532327@qq.com>
2021-01-24 16:48:00 +0800

    update: use @vue/cli to replace parcel

--- src/app.js
+++ src/app.js
@@ -1,83 +1,84 @@
-import Vue from "vue";
-import Button from "./button";
-import Icon from "./icon"
-import Group from "./button-group"
-import Input from "./input";
-import Row from "./row"
-import Col from "./col"
-import Layout from "./layout"
-import Header from "./header"
-import Content from "./content"
-import Sider from "./sider"
-import Footer from "./footer"
-import Toast from "./toast"
-import Tabs from "./tabs"
-import TabsHead from "./tabs-head"
-import TabsItem from "./tabs-item"
-import TabsBody from "./tabs-body"
-import TabsPane from "./tabs-pane"
-import TPopover from "./popover"
-import TCollapse from "./collapse"
-import TCollapseItem from "./collapse-item"
-import plugin from "./plugin";
+import Vue from 'vue';
+import Button from './button';
+import Icon from './icon';
+import Group from './button-group';
+import Input from './input';
+import Row from './row';
+import Col from './col';
+import Layout from './layout';
+import Header from './header';
+import Content from './content';
+import Sider from './sider';
+import Footer from './footer';
+import Toast from './toast';
+import Tabs from './tabs';
+import TabsHead from './tabs-head';
+import TabsItem from './tabs-item';
+import TabsBody from './tabs-body';
+import TabsPane from './tabs-pane';
+import TPopover from './popover';
+import TCollapse from './collapse';
+import TCollapseItem from './collapse-item';
+import plugin from './plugin';
+import Cascader from '@/cascader';
 
 Vue.component('t-button', Button);
 Vue.component('t-icon', Icon);
 Vue.component('t-button-group', Group);
 Vue.component('t-input', Input);
 Vue.component('t-row', Row);
-Vue.component('t-col', Col)
-Vue.component('t-layout', Layout)
-Vue.component('t-header', Header)
-Vue.component('t-content', Content)
-Vue.component('t-sider', Sider)
-Vue.component('t-footer', Footer)
-Vue.component('t-toast', Toast)
-Vue.component('t-tabs',Tabs)
-Vue.component('t-tabs-head',TabsHead)
-Vue.component('t-tabs-item',TabsItem)
-Vue.component('t-tabs-body',TabsBody)
-Vue.component('t-tabs-pane',TabsPane)
-Vue.component('t-popover',TPopover)
-Vue.component('t-collapse',TCollapse)
-Vue.component('t-collapse-item',TCollapseItem)
-
-Vue.use(plugin)
+Vue.component('t-col', Col);
+Vue.component('t-layout', Layout);
+Vue.component('t-header', Header);
+Vue.component('t-content', Content);
+Vue.component('t-sider', Sider);
+Vue.component('t-footer', Footer);
+Vue.component('t-toast', Toast);
+Vue.component('t-tabs', Tabs);
+Vue.component('t-tabs-head', TabsHead);
+Vue.component('t-tabs-item', TabsItem);
+Vue.component('t-tabs-body', TabsBody);
+Vue.component('t-tabs-pane', TabsPane);
+Vue.component('t-popover', TPopover);
+Vue.component('t-collapse', TCollapse);
+Vue.component('t-collapse-item', TCollapseItem);
+Vue.component('t-cascader', Cascader);
+Vue.use(plugin);
 
 new Vue({
-    el: '#app',
-    data: {
-        selectedTab: ['2','1'],
-        selectedTab1: ['sports'],
-        loading1: false,
-        loading2: true,
-        loading3: false,
-        message: 'h1'
+  el: '#app',
+  data: {
+    selectedTab: ['2', '1'],
+    selectedTab1: ['sports'],
+    loading1: false,
+    loading2: true,
+    loading3: false,
+    message: 'h1'
+  },
+  created() {
+  },
+  methods: {
+    showToast1() {
+      this.showToast('top');
     },
-    created() {
+    showToast2() {
+      this.showToast('middle');
     },
-    methods: {
-        showToast1() {
-            this.showToast('top')
+    showToast3() {
+      this.showToast('bottom');
+    },
+    showToast(position) {
+      this.$toast(`你的智商目前为 ${parseInt(Math.random() * 100)}。您的智商需要付费`, {
+        position,
+        enableHtml: false,
+        closeButton: {
+          text: '已充值',
+          callback() {
+            console.log('他说已经付费了');
+          }
         },
-        showToast2() {
-            this.showToast('middle')
-        },
-        showToast3() {
-            this.showToast('bottom')
-        },
-        showToast(position) {
-            this.$toast(`你的智商目前为 ${parseInt(Math.random()*100)}。您的智商需要付费`, {
-                position,
-                enableHtml: false,
-                closeButton: {
-                    text: '已充值',
-                    callback() {
-                        console.log('他说已经付费了');
-                    }
-                },
-                autoClose: 3,
-            })
-        },
-    }
-})
+        autoClose: 3,
+      });
+    },
+  }
+});
--- src/button.vue
+++ src/button.vue
@@ -1,72 +1,98 @@
 <template>
-        <button class="t-button" :class="{[`icon-${iconPosition}`]: true}"
-                @click="$emit('click')">
-            <t-icon class="icon" v-if="icon && !loading" :name="icon"></t-icon>
-            <t-icon name="loading" v-if="loading" class="loading icon"></t-icon>
-            <div class="content">
-                <slot></slot>
-            </div>
-        </button>
+  <button class="t-button" :class="{[`icon-${iconPosition}`]: true}"
+          @click="$emit('click')">
+    <t-icon class="icon" v-if="icon && !loading" :name="icon"></t-icon>
+    <t-icon name="loading" v-if="loading" class="loading icon"></t-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-    import Icon from './icon'
-  export default {
-        name: 'TButton',
-    components: {
-      't-icon' : Icon
+import Icon from './icon';
+
+export default {
+  name: 'TButton',
+  components: {
+    't-icon': Icon
+  },
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
     },
-    props: {
-      icon:{},
-      loading:{
-        type: Boolean,
-        default: false
-      },
-      iconPosition:{
-        type: String,
-        default: 'left',
-        validator(value) {
-          return !(value !== 'left' && value !== 'right');
-        }
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return !(value !== 'left' && value !== 'right');
       }
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    $font-size: 14px;
-    $button-height: 32px;
-    $button-bg: white;
-    $button-active-bg: #eee;
-    $border-radius: 4px;
-    $color:#333;
-    $border-color: #999;
-    $border-color-hover: #666;
-    @keyframes spin {
-        0%{transform: rotate(0deg);}
-        100% {transform: rotate(360deg);}
-    }
-    .t-button {
-        font-size: $font-size;
-        height: $button-height;
-        padding: 0 1em;
-        border-radius: $border-radius;
-        border: 1px solid $border-color;
-        background: $button-bg;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        vertical-align: middle;
-        &:hover {border-color: $border-color-hover;}
-        &:active {background-color: $button-active-bg;}
-        &:focus {outline: none;}
-        > .content {order: 1;}
-        > .icon {order: 1; margin-right: .1em;}
+@import "_var";
 
-        &.icon-right {
-            > .content {order: 1;}
-            > .icon {order: 2;margin-left: .1em;margin-right: 0;}
-        }
-        .loading {animation: spin 2s infinite linear;}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.t-button {
+  font-size: $font-size;
+  height: $button-height;
+  padding: 0 1em;
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+
+  &:hover {
+    border-color: $border-color-hover;
+  }
+
+  &:active {
+    background-color: $button-active-bg;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  > .content {
+    order: 1;
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
     }
+
+    > .icon {
+      order: 2;
+      margin-left: .1em;
+      margin-right: 0;
+    }
+  }
+
+  .loading {
+    animation: spin 2s infinite linear;
+  }
+}
 </style>
